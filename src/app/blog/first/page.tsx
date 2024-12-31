b21c7c2a28e847780ea22a1768ecb6606d4927c0
Naman Ajmera <cool.naman.ajmera@gmail.com>
2024-12-31 07:32:21 +0530

    Added: Nested Routes.

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,6 @@
+const First = () => {
+    return (
+        <h1>First</h1>
+    )
+}
+export default First;
---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,6 @@
+const Second = () => {
+    return (
+        <h1>Second</h1>
+    )
+}
+export default Second;
