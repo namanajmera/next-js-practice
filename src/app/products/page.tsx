694bdf8300c0a00bb76f507dfd723ef15328e4e1
Naman Ajmera <cool.naman.ajmera@gmail.com>
2025-01-01 21:16:09 +0530

    Added: Using the Link to Navigate.

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,8 +1,18 @@
+import Link from 'next/link';
 import React from 'react'
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <div>
+      <h2>Products</h2>
+      <ul>
+        <li><Link href={'/products/1'}>Product 1</Link></li>
+        <li><Link href={'/products/2'}>Product 2</Link></li>
+        <li><Link href={'/products/3'}>Product 3</Link></li>
+        <li><Link href={'/products/4'}>Product 4</Link></li>
+        <li><Link href={'/products/5'}>Product 5</Link></li>
+      </ul>
+    </div>
   )
 }
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link';
 import React from 'react'
 
 const Header = () => {
     return (
-        <div className='bg-red-400'>Header</div>
+        <div className='bg-red-400 flex justify-between p-4 w-full'>
+            <p>Header</p>
+            <p><Link href={'/'}>Home</Link></p>
+        </div>
     )
 }
 
